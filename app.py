cf56caf1f4a0b83b982adae984609746a74c86f9
AdityaKanthManne <139074539+AdityaKanthManne@users.noreply.github.com>
2026-03-06 16:57:03 -0500

    Update

---
+++ app.py
@@ -0,0 +1,20 @@
+def print_todo(list_of_todo):
+    for i in list_of_todo:
+        print(i)
+    return
+def add_todo(list_of_todo,a):
+    list_of_todo.append(a)
+    print_todo(list_of_todo)
+    return
+def del_todo(list_of_todo,a):
+    list_of_todo.append(a)
+    print_todo(list_of_todo)
+    return
+
+list_of_todo=[]
+a= input("Tell me what you want to do?\n")
+for i in list_of_todo:
+    if i==a:
+    elseif i!=a:
+
+print(list_of_todo)
