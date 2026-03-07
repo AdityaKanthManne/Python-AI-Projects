c88b81b758a38ff65ad95de6f3114c9515467ae4
AdityaKanthManne <139074539+AdityaKanthManne@users.noreply.github.com>
2026-03-06 21:53:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
 def print_todo(list_of_todo):
-    for i in list_of_todo:
-        print(i)
+    print(list_of_todo)
     return
-def add_todo(list_of_todo,a):
-    list_of_todo.append(a)
+def add_todo(list_of_todo,todo):
+    list_of_todo.append(todo)
+    print(list_of_todo)
     print_todo(list_of_todo)
     return
-def del_todo(list_of_todo,a):
-    list_of_todo.append(a)
+def del_todo(list_of_todo,todo):
+    list_of_todo.append(todo)
     print_todo(list_of_todo)
     return
 
 list_of_todo=[]
-a= input("Tell me what you want to do?\n")
-for i in list_of_todo:
-    if i==a:
-    elseif i!=a:
 
-print(list_of_todo)
+
+while True:
+    a = input("Type add or show?\n")
+    match a:
+        case "add":
+            todo=input("please add a Todo")
+            add_todo(list_of_todo,todo)
+            print(list_of_todo)
+        case "show":
+            print_todo(list_of_todo)
+        case "exit":
+            break
+
+
+
+
+
